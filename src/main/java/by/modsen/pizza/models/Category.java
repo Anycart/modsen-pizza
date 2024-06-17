6eb5394df36e0350a9a5d6080885826e7c6c5712
Kirill <justmymail0003@gmail.com>
2024-06-17 15:45:38 +0300

    1.0 entities were created and migrations through the flyway were configured

---
+++ src/main/java/by/modsen/pizza/models/Category.java
@@ -0,0 +1,30 @@
+package by.modsen.pizza.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> productList;
+}
---
+++ src/main/java/by/modsen/pizza/models/User.java
@@ -0,0 +1,33 @@
+package by.modsen.pizza.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orderList;
+}
