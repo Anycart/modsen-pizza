8fbfed897627ca630d585ddcb29410338eb43eed
Danik <daniilmarcenko44@gmail.com>
2024-06-23 23:12:23 +0300

    Business_Logic_Projecting

--- src/main/java/com/modsen/pizza/dto/ProductDto.java
+++ src/main/java/com/modsen/pizza/dto/ProductDto.java
@@ -1,7 +1,18 @@
 package com.modsen.pizza.dto;
 
+import com.modsen.pizza.entity.Category;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Data
 public class ProductDto {
+    @Min(value = 0)
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String description;
+    @Min(value = 0)
+    private Double price;
 }
